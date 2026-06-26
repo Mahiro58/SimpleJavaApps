fa8b9b2f0204f6286a949604d191d261dde58532
Mahiro58 <mr.shmitu@gmail.com>
2026-06-26 21:29:02 +0200

    Main app loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
-        System.out.println("Hello World.");
+        boolean isOn = true;
+        Scanner input = new Scanner(System.in);
+        while (isOn) {
+            System.out.flush();
+            System.out.println("Library");
+            System.out.println("Welcome! What would you like to do?");
+            System.out.println("1. Dodaj ksiazke.");
+            System.out.println("2. Usun ksiazke.");
+            System.out.println("3. Wypozycz ksiazke.");
+            System.out.println("4. Zwroc ksiazke.");
+            System.out.println("5. Lista ksiazek.");
+            System.out.println("6. Szukaj ksiazki");
+            System.out.println("7. Zapisz do pliku.");
+            System.out.println("8. Wczytaj z pliku.");
+            System.out.println("9. Wyjscie.");
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+
+
+                case 9:
+                    isOn = false;
+            }
+
+
+        }
     }
 }
